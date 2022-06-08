262fdc83b863ec93b2a7d4a14db675a893d2650b
Eugene Ivanov <most1360@yandex.ru>
2022-06-08 14:41:04 +0300

    Done

--- app/src/main/java/com/example/threadshandlers/MainActivity.kt
+++ app/src/main/java/com/example/threadshandlers/MainActivity.kt
@@ -5,7 +5,16 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity(){
+    /*
+    a) Handler - это механизм, который позволяет работать с очередью сообщений.
+    Он привязан к конкретному потоку (thread) и работает с его очередью.
 
+    Handler дает нам две интересные и полезные возможности:
+    1) реализовать отложенное по времени выполнение кода
+    2) выполнение кода не в своем потоке
+
+    Handler помещает сообщения в очередь.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
